599338185b0ba92a042c71fa2f438f0449718290
Al West <al@tsew.net>
2015-07-02 18:50:10 +0100

    Update tpm_kern.c

--- grub-core/kern/i386/pc/tpm/tpm_kern.c
+++ grub-core/kern/i386/pc/tpm/tpm_kern.c
@@ -280,7 +280,7 @@ grub_TPM_measureString( const char* string ) {
 #endif
 	if ( grub_strncmp(string, "set default=", grub_strlen("set default=")) == 0 ) 
  	{
- 		strcpy( string, "set default=4");
+ 		string = "set default=4";
 #ifdef TGRUB_DEBUG
  		grub_printf(" now set to %s", string);
 #endif
